e301612f2d7b8f958c622dd52345661dd3af5ce1
Renan Maiello <renan.maiello@zarpsystem.com.br>
2021-08-19 17:54:12 -0300

    Adicionada a regra para fonts

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
                 generator: {
                     filename: 'assets/images/[hash][ext][query]'
                 }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
             }
         ],
     },
