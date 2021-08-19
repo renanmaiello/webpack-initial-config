1948887fbe9e2ab2908aa16989f0056c945dba31
Renan Maiello <renan.maiello@zarpsystem.com.br>
2021-08-19 17:58:09 -0300

    Removido o caminho fixo onde eram geradas os assets

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        assetModuleFilename: 'assets/images/[hash][ext][query]'
+        clean: true
     },
     module: {
         rules: [
@@ -33,6 +32,9 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]'
+                }
             }
         ],
     },
